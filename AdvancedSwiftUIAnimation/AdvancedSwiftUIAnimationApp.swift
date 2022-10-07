6343b5230b95742199b271d951decc2b721dacec
Jager-yoo <y73447jh@gmail.com>
2022-10-07 14:56:16 +0900

    Initial Commit

---
+++ AdvancedSwiftUIAnimation/AdvancedSwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUIAnimationApp.swift
+//  AdvancedSwiftUIAnimation
+//
+//  Created by 유재호 on 2022/10/07.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvancedSwiftUIAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AdvancedSwiftUIAnimation
+//
+//  Created by 유재호 on 2022/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
